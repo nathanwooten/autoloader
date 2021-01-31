8a9baf1e4644704e149f76d1d814dedb2330a28c
Nathan Wooten <a8gid24akl@gmail.com>
2021-01-31 10:25:14 -0500

    added index.php, version safe include

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Version safe include, the index file is always here, despite the version, to load the autoloader class
+ */
+
+require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'Autoloader.php';
+
+?>
