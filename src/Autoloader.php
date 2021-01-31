37d9c8e29c77fe91c213482ca6ca21cba31007ef
Nathan Wooten <a8gid24akl@gmail.com>
2021-01-31 04:14:41 -0500

    first commit version 2

---
+++ src/Autoloader.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Pf\Autoloader;
+
+	$class = __NAMESPACE__ . '\\' . rtrim( basename( __FILE__ ), '.php' );
+if ( ! class_exists( $class ) ) {
+
+class Autoloader {
+
+	public $init = false;
+
+	public $vendor;
+	public $directory;
+
+	public $exists = 'file_exists';
+
+	public static function factory()
+	{
+
+		$instance = new static;
+		$instance->init( ...func_get_args() );
+
+		return $instance;
+
+	}
+
+	public function init() {
+
+		if ( ! $this->init ) {
+
+				$args = func_get_args();
+
+			$configure = empty( $args ) ? [] : $args[0];
+			if ( ! empty( $configure ) ) {
+				$this->configure( $configure );
+			}
+
+			spl_autoload_register( [ $this, 'autoload' ] );
+
+			$this->init = true;
+
+		}
+
+		return $this;
+
+	}
+
+
+	public function configure( array $config )
+	{
+
+		$result = [];
+
+		foreach ( $config as $methodName => $args ) {
+
+			$callback = [ $this, $methodName ];
+			if ( ! is_callable( $callback ) ) {
+
+				return false;
+			}
+
+			$result[] = $callback( ...array_values( $args ) );
+		}
+
+		return $result;
+
+	}
+
+	public function load( $vendor, $directory ) {
+
+		$this->setVendor( $vendor );
+		$this->setBase ( $directory );
+
+		$this->init();
+
+	}
+
+	public function setVendor( $vendor )
+	{
+
+		$this->vendor = $vendor;
+
+	}
+
+	public function getVendor()
+	{
+
+		return $this->vendor;
+
+	}
+
+	public function setBase( $directory )
+	{
+
+		$this->directory = $directory;
+
+	}
+
+	public function getBase()
+	{
+
+		return $this->directory;
+
+	}
+
+	public function autoload( $interface ) {
+
+		if ( ! $this->init ) {
+			$this->init();
+		}
+
+		$file = $this->locate( $interface );
+		if ( $file ) {
+
+			require_once $file;
+
+			return $interface;
+		}
+
+	}
+
+	public function locate( $interface )
+	{
+
+		$vendorName = $this->normalize( $this->getVendor() );
+		$directory = $this->normalize( $this->getBase() );
+
+		$file = str_replace(
+
+			$vendorName,
+			$directory,
+
+			$interface
+
+		) . '.php';
+
+			$exists = $this->exists;
+		if ( $exists( $file ) ) {
+
+			return $file;
+		}
+
+	}
+
+	public function autoloadArray( array $array )
+	{
+
+		$interfaces = [];
+
+		while( $array ) {
+			$interface = array_shift( $array );
+
+			$interfaces[] = $this->autoload( $interface );
+		}
+
+		return $interfaces;
+
+	}
+
+	public function normalize( $item )
+	{
+
+		$item = str_replace( ['\\', '/'], DIRECTORY_SEPARATOR, $item );
+
+		$item = rtrim( $item, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR;
+
+		return $item;
+
+	}
+
+}
+}
