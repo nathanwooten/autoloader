a59d6adcaefe726acb4c657cb60b01e94c5a9f12
nw <55707935+nathanwooten@users.noreply.github.com>
2021-03-20 13:24:34 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -2,14 +2,46 @@
 
 namespace Pf\Autoloader;
 
-function matchSpace( $alias, $directories = [] ) {
+function matchSpace( $alias, $directories = [] )
+{
   $separator = DIRECTORY_SEPARATOR;
-  $alias = trim( $alias, '/', '\\' );
-  $alias = str_replace( ['\\', '/', $spearator, $alias );
-  $aliasArray = explode( $separator );
+  $alias = normalize( 'trim', $alias );
+  $alias = normalize( 'replace', $alias, $separator );
+  $aliasArray = explode( $separator, $alias );
   $path = '';
   foreach ( $aliasArray as $key => $name ) {
     $path .= $name . $separator
-    foreach ( explode( $separator, str_replace( '\\', '
+    $directory = $directories[$key];
   }
 }
+
+function normalize( $dest, $item, ...$args )
+{
+
+  switch( $dest ) {
+
+    case 'trim':
+
+      if ( ! empty( $args ) ) {
+         $trim = '\\' . $args[ 0 ] . 'trim';
+      } else {
+        $trim = '\\' . 'trim'; 
+      }
+      $item = $trim( $item, '/', '\\' );
+      break;
+
+    case 'replace':
+
+      $item = str_replace( [ '\\', '/' ], isset( $args[0] ) ? $args[0], DIRECTORY_SEPARATOR, $item );
+      break;
+
+  }
+
+  return  $item;
+
+}
+
+
+
+
+
