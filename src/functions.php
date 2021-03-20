85eec9e6ff26964a529fe2b7a5132934d751412c
nw <55707935+nathanwooten@users.noreply.github.com>
2021-03-20 16:31:36 -0400

    add count to match function

--- src/functions.php
+++ src/functions.php
@@ -45,6 +45,7 @@ function matchSpace( $alias, $directories = [] )
     } elseif ( $count === $matchCount && 0 < $count ) {
       $matches[] = $directory;
     }
+    ++$count;
   }
   return $matches;
 
