5ad4ad842d411ad4038bba20fc908ef9734a9c9e
nw <55707935+nathanwooten@users.noreply.github.com>
2021-03-20 12:39:06 -0400

    Delete require.php

--- src/require.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
-
-require_once $dir . 'Autoloader.php';
-require_once $dir . 'AutoloaderAbstract.php';
-require_once $dir . 'AutoloaderPackage.php';
-require_once $dir . 'AutoloaderSpace.php';
-require_once $dir . 'AutoloaderDir.php';
