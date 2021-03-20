1cb822ab656e0d5acc91ff49a19a503126496087
nw <55707935+nathanwooten@users.noreply.github.com>
2021-03-20 12:54:59 -0400

    have an alias array

--- src/functions.php
+++ src/functions.php
@@ -3,5 +3,9 @@
 namespace Pf\Autoloader;
 
 function match( $alias, $directories = [] ) {
+  $separator = DIRECTORY_SEPARATOR;
+  $alias = trim( $alias, '/', '\\' );
+  $alias = str_replace( '\\/', $separator, $alias );
+  $aliasArray = explode( DIRECTORY_SEPARATOR );
   
 }
